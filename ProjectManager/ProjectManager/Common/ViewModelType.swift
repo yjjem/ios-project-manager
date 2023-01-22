fb37b94682e3edb63f5441d4ec086ad524e37fc4
yjjem <marksim0606@gmail.com>
2023-01-22 20:09:49 +0900

    feat: add ViewModelType

---
+++ ProjectManager/ProjectManager/Common/ViewModelType.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModelType.swift
+//  ProjectManager
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+protocol ViewModelType {
+    associatedtype Input
+    associatedtype Output
+    
+    func transform(input: Input) -> Output
+}
